0e30df97e790b94e2474963a2ace9b62e5e87936
Denis Subbotin <denis@subbot.in>
2022-11-01 18:19:50 +0300

    cors

--- http.go
+++ http.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func registerHandlers(e *echo.Echo, h *handler) {
+	cors := middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST"},
+	}
 	bridge := e.Group("/bridge")
 	bridge.GET("/events", h.EventRegistrationHandler)
-	bridge.POST("/message", h.SendMessageHandler)
+	bridge.POST("/message", h.SendMessageHandler, middleware.CORSWithConfig(cors))
 }
