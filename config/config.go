91dcea53b39c3b15fd0de29ee7282fb18646a1a3
Zakhar Petukhov <zakharpetukhov@protonmail.com>
2024-04-16 20:48:38 +0800

    create a func to refresh unlimited tokens

--- config/config.go
+++ config/config.go
@@ -1,7 +1,10 @@
 package config
 
 import (
+	"encoding/json"
 	"log"
+	"os"
+	"time"
 
 	"github.com/caarlos0/env/v6"
 )
@@ -13,7 +16,7 @@ var Config = struct {
 	CorsEnable            bool     `env:"CORS_ENABLE"`
 	HeartbeatInterval     int      `env:"HEARTBEAT_INTERVAL" envDefault:"10"`
 	RPSLimit              int      `env:"RPS_LIMIT" envDefault:"1"`
-	RateLimitsByPassToken []string `env:"RATE_LIMITS_BY_PASS_TOKEN"`
+	RateLimitsByPassToken []string `env:"RATE_LIMITS_BY_PASS_TOKEN"` // TODO: remove this env, read from file
 	ConnectionsLimit      int      `env:"CONNECTIONS_LIMIT" envDefault:"50"`
 	SelfSignedTLS         bool     `env:"SELF_SIGNED_TLS" envDefault:"false"`
 }{}
@@ -21,5 +24,33 @@ var Config = struct {
 func LoadConfig() {
 	if err := env.Parse(&Config); err != nil {
 		log.Fatalf("config parsing failed: %v\n", err)
+	}
+
+	go refreshUnlimitedTokens()
+}
+
+func refreshUnlimitedTokens() {
+	type UnlimitedTokens struct {
+		Tokens []string `json:"tokens"`
+	}
+	refresh := func() []string {
+		file, err := os.ReadFile("config/unlimited_tokens.json")
+		if err != nil {
+			log.Printf("failed to read unlimited tokens file: %v", err)
+			return nil
+		}
+		var tokens UnlimitedTokens
+		if err = json.Unmarshal(file, &tokens); err != nil {
+			log.Printf("failed to convert unlimited tokens: %v", err)
+			return nil
+		}
+		return tokens.Tokens
+	}
+	for {
+		tokens := refresh()
+		if tokens != nil {
+			Config.RateLimitsByPassToken = tokens
+		}
+		time.Sleep(time.Minute * 5)
 	}
 }
