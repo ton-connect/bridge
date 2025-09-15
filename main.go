1fde7002f3e965b800d5cf2b6eb68b14475daae4
callmedenchick <dvolkov@walletteam.org>
2025-09-15 21:40:03 +0200

    keep only index profile

--- main.go
+++ main.go
@@ -44,10 +44,6 @@ func main() {
 	mux.Handle("/metrics", promhttp.Handler())
 	if config.Config.PprofEnabled {
 		mux.HandleFunc("/debug/pprof/", pprof.Index)
-		mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-		mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
-		mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-		mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
 	}
 	go func() {
 		log.Fatal(http.ListenAndServe(":9103", mux))
