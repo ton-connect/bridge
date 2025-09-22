d3f6197fdbc0190266ba2672c59a465b13534c58
callmedenchick <dvolkov@walletteam.org>
2025-09-22 23:49:49 +0200

    move message_cache to internal/storage

--- internal/v1/storage/storage.go
+++ internal/v1/storage/storage.go
@@ -2,8 +2,17 @@ package storage
 
 import (
 	"context"
+	"time"
 
 	"github.com/tonkeeper/bridge/datatype"
+	common_storage "github.com/tonkeeper/bridge/internal/storage"
+)
+
+var (
+	// ExpiredCache    = common_storage.NewMessageCache(config.Config.EnableExpiredCache, time.Hour)
+	// TransferedCache = common_storage.NewMessageCache(config.Config.EnableTransferedCache, time.Minute)
+	ExpiredCache    = common_storage.NewMessageCache(time.Hour)
+	TransferedCache = common_storage.NewMessageCache(time.Minute)
 )
 
 type Storage interface {
