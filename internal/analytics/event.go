c009be361fdf863966e3c7ca712eb00e0c0fc0cd
callmedenchick <dvolkov@walletteam.org>
2025-11-20 22:28:40 +0100

    SendBatch + minor refactoring

--- internal/analytics/event.go
+++ internal/analytics/event.go
@@ -2,146 +2,64 @@ package analytics
 
 import "github.com/ton-connect/bridge/tonmetrics"
 
-// Event is a small command that knows how to emit itself via TonMetrics.
-type Event struct {
-	dispatch func(tonmetrics.AnalyticsClient)
+// NewBridgeMessageExpiredEvent builds a bridge-message-expired event.
+func NewBridgeMessageExpiredEvent(clientID, traceID string, messageID int64, messageHash string) interface{} {
+	// Event creation is delegated to a temporary client to fill common fields.
+	// In production, these events are collected and sent in batches.
+	client := getEventBuilder()
+	return client.CreateBridgeMessageExpiredEvent(clientID, traceID, "", messageID, messageHash)
 }
 
-// Dispatch sends the event through the provided client.
-func (e Event) Dispatch(client tonmetrics.AnalyticsClient) {
-	if e.dispatch != nil {
-		e.dispatch(client)
-	}
+func NewBridgeMessageSentEvent(clientID, traceID string, messageID int64, messageHash string) interface{} {
+	client := getEventBuilder()
+	return client.CreateBridgeMessageSentEvent(clientID, traceID, "", messageID, messageHash)
 }
 
-// NewBridgeMessageExpiredEvent builds an Event for a message expiration.
-func NewBridgeMessageExpiredEvent(clientID, traceID string, messageID int64, messageHash string) Event {
-	return Event{
-		dispatch: func(client tonmetrics.AnalyticsClient) {
-			client.SendEvent(client.CreateBridgeMessageExpiredEvent(
-				clientID,
-				traceID,
-				"",
-				messageID,
-				messageHash,
-			))
-		},
-	}
+func NewBridgeMessageReceivedEvent(clientID, traceID, requestType string, messageID int64, messageHash string) interface{} {
+	client := getEventBuilder()
+	return client.CreateBridgeMessageReceivedEvent(clientID, traceID, requestType, messageID, messageHash)
 }
 
-func NewBridgeMessageSentEvent(clientID, traceID string, messageID int64, messageHash string) Event {
-	return Event{
-		dispatch: func(client tonmetrics.AnalyticsClient) {
-			client.SendEvent(client.CreateBridgeMessageSentEvent(
-				clientID,
-				traceID,
-				"",
-				messageID,
-				messageHash,
-			))
-		},
-	}
+func NewBridgeMessageValidationFailedEvent(clientID, traceID, requestType, messageHash string) interface{} {
+	client := getEventBuilder()
+	return client.CreateBridgeMessageValidationFailedEvent(clientID, traceID, requestType, messageHash)
 }
 
-func NewBridgeMessageReceivedEvent(clientID, traceID, requestType string, messageID int64, messageHash string) Event {
-	return Event{
-		dispatch: func(client tonmetrics.AnalyticsClient) {
-			client.SendEvent(client.CreateBridgeMessageReceivedEvent(
-				clientID,
-				traceID,
-				requestType,
-				messageID,
-				messageHash,
-			))
-		},
-	}
+func NewBridgeVerifyEvent(clientID, traceID, verificationResult string) interface{} {
+	client := getEventBuilder()
+	return client.CreateBridgeVerifyEvent(clientID, traceID, verificationResult)
 }
 
-func NewBridgeMessageValidationFailedEvent(clientID, traceID, requestType, messageHash string) Event {
-	return Event{
-		dispatch: func(client tonmetrics.AnalyticsClient) {
-			client.SendEvent(client.CreateBridgeMessageValidationFailedEvent(
-				clientID,
-				traceID,
-				requestType,
-				messageHash,
-			))
-		},
-	}
+func NewBridgeEventsClientSubscribedEvent(clientID, traceID string) interface{} {
+	client := getEventBuilder()
+	return client.CreateBridgeEventsClientSubscribedEvent(clientID, traceID)
 }
 
-func NewBridgeVerifyEvent(clientID, traceID, verificationResult string) Event {
-	return Event{
-		dispatch: func(client tonmetrics.AnalyticsClient) {
-			client.SendEvent(client.CreateBridgeVerifyEvent(
-				clientID,
-				traceID,
-				verificationResult,
-			))
-		},
-	}
+func NewBridgeEventsClientUnsubscribedEvent(clientID, traceID string) interface{} {
+	client := getEventBuilder()
+	return client.CreateBridgeEventsClientUnsubscribedEvent(clientID, traceID)
 }
 
-func NewBridgeEventsClientSubscribedEvent(clientID, traceID string) Event {
-	return Event{
-		dispatch: func(client tonmetrics.AnalyticsClient) {
-			client.SendEvent(client.CreateBridgeEventsClientSubscribedEvent(
-				clientID,
-				traceID,
-			))
-		},
-	}
+// NewBridgeConnectEstablishedEvent builds a bridge-connect-established event.
+func NewBridgeConnectEstablishedEvent(clientID, traceID string, connectDuration int) interface{} {
+	client := getEventBuilder()
+	return client.CreateBridgeConnectEstablishedEvent(clientID, traceID, connectDuration)
 }
 
-func NewBridgeEventsClientUnsubscribedEvent(clientID, traceID string) Event {
-	return Event{
-		dispatch: func(client tonmetrics.AnalyticsClient) {
-			client.SendEvent(client.CreateBridgeEventsClientUnsubscribedEvent(
-				clientID,
-				traceID,
-			))
-		},
-	}
+// NewBridgeRequestSentEvent builds a bridge-request-sent event.
+func NewBridgeRequestSentEvent(clientID, traceID, requestType string, messageID int64, messageHash string) interface{} {
+	client := getEventBuilder()
+	return client.CreateBridgeRequestSentEvent(clientID, traceID, requestType, messageID, messageHash)
 }
 
-// NewBridgeConnectEstablishedEvent builds an Event for when connection is established.
-func NewBridgeConnectEstablishedEvent(clientID, traceID string, connectDuration int) Event {
-	return Event{
-		dispatch: func(client tonmetrics.AnalyticsClient) {
-			client.SendEvent(client.CreateBridgeConnectEstablishedEvent(
-				clientID,
-				traceID,
-				connectDuration,
-			))
-		},
-	}
+// NewBridgeVerifyValidationFailedEvent builds a bridge-verify-validation-failed event.
+func NewBridgeVerifyValidationFailedEvent(clientID, traceID string, errorCode int, errorMessage string) interface{} {
+	client := getEventBuilder()
+	return client.CreateBridgeVerifyValidationFailedEvent(clientID, traceID, errorCode, errorMessage)
 }
 
-// NewBridgeRequestSentEvent builds an Event for when request is posted to /message.
-func NewBridgeRequestSentEvent(clientID, traceID, requestType string, messageID int64, messageHash string) Event {
-	return Event{
-		dispatch: func(client tonmetrics.AnalyticsClient) {
-			client.SendEvent(client.CreateBridgeRequestSentEvent(
-				clientID,
-				traceID,
-				requestType,
-				messageID,
-				messageHash,
-			))
-		},
-	}
-}
-
-// NewBridgeVerifyValidationFailedEvent builds an Event for when verify validation fails.
-func NewBridgeVerifyValidationFailedEvent(clientID, traceID string, errorCode int, errorMessage string) Event {
-	return Event{
-		dispatch: func(client tonmetrics.AnalyticsClient) {
-			client.SendEvent(client.CreateBridgeVerifyValidationFailedEvent(
-				clientID,
-				traceID,
-				errorCode,
-				errorMessage,
-			))
-		},
-	}
+// getEventBuilder returns the global analytics client for building events.
+// Events are created with common fields (bridge URL, version, etc.) filled in.
+func getEventBuilder() tonmetrics.AnalyticsClient {
+	return tonmetrics.NewAnalyticsClient()
 }
